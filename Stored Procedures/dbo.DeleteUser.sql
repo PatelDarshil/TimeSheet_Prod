bc8c8c42115ad3c1b6238140c9fddcabad1b6b8b
Darshil_Patel <darshil.patel@forartech.com>
2019-10-15 16:00:12 +0530

    Create dbo.DeleteUser.sql

---
+++ Stored Procedures/dbo.DeleteUser.sql
@@ -0,0 +1,17 @@
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[DeleteUser] 
+    @UserID int
+	
+AS
+BEGIN
+   DELETE dbo.tbl_User WHERE UserID=@UserID
+END
+GO
