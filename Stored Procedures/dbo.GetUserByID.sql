48f47036d8073d75a5715c4cd6043d481de1a2b7
DARSHIL-DOTNET$@stridely.local <DARSHIL-DOTNET$@stridely.local>
2019-10-15 16:37:04 +0530

    'commit'

---
+++ Stored Procedures/dbo.GetUserByID.sql
@@ -0,0 +1,16 @@
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[GetUserByID]
+	@UserID INT
+AS
+BEGIN
+	SELECT * FROM dbo.tbl_User WHERE UserID=@UserID
+END
+GO
